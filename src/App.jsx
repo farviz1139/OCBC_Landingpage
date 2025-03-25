4ab1050407ac8a4a56c6ddea732637d65a90c66f
Farviz14 <2301186I@student.tp.edu.sg>
2025-03-25 14:44:01 +0800

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import NavBar from './components/NavBar'
+import Email from './email'
+import Properties from './components/Properties'
+import Property_2 from './components/Property_2'
+import People from './people'
+import Signup from './signup'
+
+
+
+
 function App() {
-  return <></>;
+  return <>
+    <NavBar/>
+    <Email/>
+    <Properties/>
+    <Property_2/>
+    <People />
+    <Signup />
+
+
+  </>;
 }
 
 export default App;
