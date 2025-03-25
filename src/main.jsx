80a1af2efd9aae921c7243f6332d71f9de418496
kohye <kohyechyang11@gmail.com>
2025-03-25 16:38:29 +0800

    cal

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Calculator from './calculator.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Calculator />
   </StrictMode>,
 )
